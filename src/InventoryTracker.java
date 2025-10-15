ecf08a653155072529a7c2466e19c60825a36362
Cyber410 <devsagwal96@gmail.com>
2025-10-15 14:21:02 -0400

    Implemented stock checking by Dev Kumar

--- src/InventoryTracker.java
+++ src/InventoryTracker.java
@@ -2,4 +2,8 @@ public class InventoryTracker {
     public static void main(String[] args) {
         System.out.println("Inventory Tracker initialized.");
     }
+
+     public void checkStockLevel() {
+        System.out.println("Checking stock levels...");
+    }
 }
